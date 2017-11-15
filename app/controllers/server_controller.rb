33227cb8480e18914fe8fc52facc3b325dd0292c
Shuhail Kadavath <skadavath@skadavath-m01.vmware.com>
2017-11-15 15:07:20 +0530

    report

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -3,9 +3,19 @@ class ServerController < ApplicationController
   def beat
     p 'in heartbeat fn'
     check_st
+    p = argv[0]
+    if p is nil?
+
+      report
+
+    end
   end
 
   def check_st
     p 'in check status'
+  end
+
+  def report
+
   end
 end
